35d3f2be1f01b73b2a95f3ec84de2fa0889dc267
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-04 00:00:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength <= 400) {
+    return "This one is on me!";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+if (rideLength >2500) {
+    return "No can do.";
+} else if (rideLength >= 2000) {
+    return "I will gladly take your thirty bucks.";
+}
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good.": "No go.";
 }
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip) {
+    case 'generous' :
+        return 'Thank you so much.';
+        break;
+    case 'not as generous' :
+        return 'Thank you.';
+        break;
+    default:
+        return 'Bye.';
+        break;
+}}
